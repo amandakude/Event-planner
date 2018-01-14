dc7131d1a03200a23a50e7f3d348fb1c5b683c6d
amandakude <35407393+amandakude@users.noreply.github.com>
2018-01-14 18:34:42 +0200

    Update event.blade.php

--- resources/views/events/event.blade.php
+++ resources/views/events/event.blade.php
@@ -1,10 +1,10 @@
-<div class="blog-post">
-    <h2 class="blog-post-title">
+<div class="event-post">
+    <h2 class="event-title">
         <a href='/events/{{ $event->id }}'> 
             {{ $event->title }}
         </a>
     </h2>
-            <p class="blog-post-meta">{{ $event->date }} {{ $event->time }}</p>
+            <p class="event-meta">{{ $event->date }} {{ $event->time }}</p>
 
             <p>{{ $event->body }}</p>
 </div>
